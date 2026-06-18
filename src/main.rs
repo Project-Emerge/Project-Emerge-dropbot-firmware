a077b86c623ed2702dfb471abd4ac3f566329120
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-06-18 17:16:14 +0200

    build: project setup from ariel-os template repository

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#![no_std]
+
+use ariel_os::{
+    debug::{ExitCode, exit},
+    log::info,
+};
+
+#[ariel_os::task(autostart)]
+async fn main() {
+    info!(
+        "Hello from main()! Running on a {} board.",
+        ariel_os::buildinfo::BOARD
+    );
+    exit(ExitCode::SUCCESS);
+}
