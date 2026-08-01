44f4f75f2602b2e81f9a8f0a1c4433a9df829875
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-08-01 12:27:57 +0200

    build: add flake and direnv for a reproducible environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Project Emerge dropbot firmware (Ariel OS / ESP32-C6)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustup
+            laze
+            espflash
+            ninja
+            pkg-config
+            udev
+            clang
+            pkgsCross.riscv32-embedded.buildPackages.gcc
+          ];
+        };
+      });
+    };
+}
